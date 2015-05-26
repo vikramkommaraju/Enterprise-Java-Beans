4a7178ef108fc5289029ba20e053cd85a9db008a
Vik <vikram.kommaraju@gmail.com>
2015-05-25 17:06:10 -0700

    Adding test cases for create/update/delete of book from Main class

--- src/test/java/github/vikram/javaee/ejb/Main.java
+++ src/test/java/github/vikram/javaee/ejb/Main.java
@@ -13,11 +13,36 @@ public class Main {
 		
 		BookEJBRemote bookEJB = (BookEJBRemote) ctx.lookup("java:global/ejb-0.0.1-SNAPSHOT/BookEJB!github.vikram.javaee.ejb.BookEJBRemote");
 		
-		 // Gets and displays all the books from the database
+		
 	    List<Book> books = bookEJB.findBooks();
+	    System.out.println("##################################################");
 	    for (Book book : books) {
 	      System.out.println(book);
 	    }
+	    System.out.println("##################################################");
+	    
+	    
+	    Book b = new Book("Test Book", 20F, "This is a test book", "1-234456-3232-12", 100, false);
+	    System.out.println("Persisting book..." + b);    
+	    b = bookEJB.createBook(b);
+	    System.out.println("Persisted book:" + b);
+	    System.out.println("##################################################");
+	    
+	    b.setDescription("Updated description for test book");
+	    b = bookEJB.updateBook(b);
+	    System.out.println("Updated book description to: " + b);
+	    System.out.println("##################################################");
+	    
+	    System.out.println("Deleting book...");
+	    bookEJB.deleteBook(b);
+	    System.out.println("Deleted book: " + b);
+	    System.out.println("##################################################");
+	    
+	    System.out.println("Getting list of all books again");
+	    for (Book book : bookEJB.findBooks()) {
+		      System.out.println(book);
+		    }
+	    
 
 	}
 
